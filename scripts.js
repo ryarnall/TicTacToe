96d12e707e622e440b449a33d6fb20d5325a5a27
ryarnall <rachelleyarnall@gmail.com>
2017-09-11 12:37:24 -0500

    Computer adds 'O' to a random empty space after human takes their turn

--- scripts.js
+++ scripts.js
@@ -1,14 +1,49 @@
-$("td").click(placeMarker);
+$("td").click(playerTurn);
 $("#restartButton").click(restartGame);
 
-function placeMarker(eventData) {
+gameState(1);
+
+function gameState(state) {
+	var message = $("p");
+
+	if (state == 1) {
+		message.text("Welcome! Place your marker to begin the game");
+	} else if (state == 2) {
+		message.text("Your turn")
+	} else if (state == 3) {
+		message.text("Comp turn");
+		computerTurn();
+	} else if (state == 4) {
+		message.text("New game! Place your marker");
+	}
+}
+
+function randomBox() {
+	var random = Math.floor(Math.random()*$("td").length);
+	return $("td").eq(random);
+}
+
+
+function playerTurn(eventData) {
 	var $clickedBox = $(eventData.target);
 	if ($clickedBox.text() == "") {
 		$clickedBox.text("X");
+		gameState(3);
+	}
+}
+
+function computerTurn() {
+	var aBox = randomBox();
+	if (aBox.text() == "") {
+		aBox.text("O");
+		gameState(2);
+	} else {
+		computerTurn();
 	}
 }
 
 
 function restartGame() {
 	$("td").empty();
+	gameState(4);
 }
