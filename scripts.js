9e5fa69ebf137a4db3b83982dc295cbd30c2f671
ryarnall <rachelleyarnall@gmail.com>
2017-09-19 12:01:57 -0500

    1st Stage AI - 1/4 chance computer will choose a spot that completes a row of 3 O's

--- scripts.js
+++ scripts.js
@@ -96,16 +96,108 @@ function playerTurn(eventData) {
 	}
 }
 
-//using randomBox(), places an "O" in the given cell (if its empty)
+
 function computerTurn() {
+	var chance = Math.floor(Math.random()*4);
+
+	if (chance == 0) {	
+		computerRandom();
+	} else {
+		computerSmart();
+	}
+
+
+}
+
+//using randomBox(), places an "O" in the given cell (if its empty)
+function computerRandom() {
 	var aBox = randomBox();
 	if (aBox.text() == "") {
-		aBox.addClass("computerMarker")
-		aBox.text("O");
+		aBox.addClass("computerMarker").text("O");
 		whoWon("computer");
 	} else {
-		computerTurn();
+		computerRandom();
 	}
+}
+
+function computerSmart() {
+	var a1 = $("#a1").text();
+	var a2 = $("#a2").text();
+	var a3 = $("#a3").text();
+
+	var b1 = $("#b1").text();
+	var b2 = $("#b2").text();
+	var b3 = $("#b3").text();
+
+	var c1 = $("#c1").text();
+	var c2 = $("#c2").text();
+	var c3 = $("#c3").text();
+
+
+	if (a1 == "" && (
+				(b1 == "O" && b1 == c1) ||
+				(a2 == "O" && a2 == a3) ||
+				(b2 == "O" && b2 == c3))) {
+		$("#a1").text("O").addClass("computerMarker");
+		gameState(7);
+
+	} else if (a2 == "" && (
+							(b2 == "O" && b2 == c2) ||
+							(a1 == "O" && a1 == a3))) {
+		$("#a2").text("O").addClass("computerMarker");
+		gameState(7);
+
+	} else if (a3 == "" && (
+							(b3 == "O" && b3 == c3) ||
+							(a1 == "O" && a1 == a2) ||
+							(b2 == "O" && b2 == c1))) {
+		$("#a3").text("O").addClass("computerMarker");
+		gameState(7);
+
+	} else if (b1 == "" && (
+							(a1 == "O" && b1 == c1) ||
+							(b2 == "O" && b2 == b3))) {
+		$("#b1").text("O").addClass("computerMarker");
+		gameState(7);
+
+	} else if (b2 == "" && ( 
+							(a2 == "O" && a2 == c2) ||
+							(b1 == "O" && b1 == a3) ||
+							(a1 == "O" && a1 == c3) ||
+							(c1 == "O" && c1 == a3))) {
+		$("#b2").text("O").addClass("computerMarker");
+		gameState(7);
+
+	} else if (b3 == "" && (
+							(a3 == "O" && a3 == c3) ||
+							(b1 == "O" && b1 == b2))) {
+		$("#b3").text("O").addClass("computerMarker");
+		gameState(7);
+
+	} else if (c1 == "" && (
+							(a1 == "O" && a1 == b1) ||
+							(c2 == "O" && c2 == c3) ||
+							(b2 == "O" && b2 == a3))) {
+		$("#c1").text("O").addClass("computerMarker");
+		gameState(7);
+
+	} else if (c2 == "" && (
+							(a2 == "O" && a2 == b2) ||
+							(c1 == "O" && c1 == c3))) {
+		$("#c2").text("O").addClass("computerMarker");
+		gameState(7);
+
+	} else if (c3 == "" && (
+							(a3 == "O" && a3 == b3) ||
+							(c1 == "O" && c1 == c2))) {
+		$("#c3").text("O").addClass("computerMarker");
+		gameState(7);
+
+	}	else {
+		computerRandom();
+	}
+
+
 }
 
 //clears the game board
